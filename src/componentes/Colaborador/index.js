533cd51c0e400f774def1f80631c38ce1cd0d06c
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2024-08-19 16:05:33 -0300

    Criando arquivo, criando states do time, para conseguir mudar cor geral

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,19 @@
+import { IoClose } from "react-icons/io5";
+import "./colaborador.css";
+
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+  return (
+    <div className="colaborador">
+      <IoClose className="deletar" onClick={aoDeletar} />
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+        <img src={colaborador.imagem} alt={colaborador.nome} />
+      </div>
+      <div className="rodape">
+        <h4>{colaborador.nome}</h4>
+        <h5>{colaborador.cargo}</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
